40f84d097726299dba5e24c9f615afb0fcd265d8
luissegovia <luis.segovia244@gmail.com>
2020-02-18 15:57:12 -0600

    Add files via upload

---
+++ js/wpsol_3417cb24716299d45b6efee6cfb2d292.js
@@ -0,0 +1,3 @@
+/*WPSOL_MINIFIED_JS*/new VueW3CValid({el:'#stm-listing-login'});new Vue({el:'#stm-listing-login',data:{loading:false,login:'',password:'',message:null,remember:0,status:'',errors:[]},methods:{logIn:function(){var vm=this;vm.loading=true;vm.message=null;var data={'login':vm.login,'password':vm.password,'remember':vm.remember};vm.errors=[];this.$http.post(currentAjaxUrl+'?action=stm_listing_login',data).then(function(response){vm.loading=false;vm.message=response.body['message'];vm.status=response.body['status'];if(response.body['errors'])
+vm.errors=response.body['errors'];if(vm.status=='success')
+location.reload();});}}});
